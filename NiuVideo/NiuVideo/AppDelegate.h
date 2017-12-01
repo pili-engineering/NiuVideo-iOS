8c077589d41294d1e23bbc5421627b3c59449a78
depthlove <suntongmian@163.com>
2017-12-01 16:32:43 +0800

    Create the NiuVideo project

---
+++ NiuVideo/NiuVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NiuVideo
+//
+//  Created by suntongmian on 2017/12/1.
+//  Copyright © 2017年 Pili Engineering, Qiniu Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NiuVideo/NiuVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NiuVideo
+//
+//  Created by suntongmian on 2017/12/1.
+//  Copyright © 2017年 Pili Engineering, Qiniu Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
